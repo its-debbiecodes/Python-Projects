935c9898d1008a18e5d53ded75c0622ad6ed600f
its-debbiecodes <debbieabegunde23@gmail.com>
2026-03-13 08:24:03 +0000

    git commit -m "Version 1.0 completed"

--- To-Do List.py
+++ To-Do List.py
@@ -1,49 +1,51 @@
 
+name = input("Please enter your name: ").title()
 def show_menu():
     print("-----YOUR TO DO LISTS-----")
-    name=input("Please enter your name: ")
-    print(f"Welcome {name}, what would you like to do?!")
+    print(f"Hi {name}, what would you like to do?!")
     print("1. View Tasks".capitalize())
     print("2. Add Tasks".capitalize())
     print("3. Remove Tasks".capitalize())
     print("4. Exit".capitalize())
 
 def main():
-    task=[]
+    tasks=[
+
+    ]
 
     while True:
         show_menu()
         choice = input("Please enter your choice (1-4): ")
         if choice == "1":
-            print("\nYour task".capitalize())
+            print("\nYour task".title())
 
-            if not task:
+            if not tasks:
                 print("Your tasks list is empty!")
                 continue
 
             else:
-                for index, task in enumerate(task):
-                    print(f"{index}. {task}")
+                for index, item in enumerate(tasks, start=1):
+                    print(f"{index}. {item}")
 
         elif choice == "2":
             new_task = input("Please enter a new task: ")
-            task.append(new_task)
+            tasks.append(new_task)
 
             print(f"{new_task} added to your tasks list")
-            for index, task in enumerate(task):
-                print(f"{index}. {task}")
+            for index, item in enumerate(tasks, start=1):
+                print(f"{index}. {item}")
 
         elif choice == "3":
-            if not task:
+            if not tasks:
                 print("Your tasks list is empty!")
                 continue
 
-            for index, task in enumerate(task):
-                print(f"{index}. {task}")
+            for index, item in enumerate(tasks, start=1):
+                print(f"{index}. {item}")
 
             try:
                 task_num = int(input("Please enter the number of task you would like to remove: "))
-                removed_task=task.pop(task_num-1)
+                removed_task=tasks.pop(task_num-1)
                 print(f"{removed_task} removed from your tasks list")
 
             except (ValueError, IndexError) as e:
@@ -58,8 +60,8 @@ def main():
             print("Please enter a valid number (1-4)")
             continue
 
-        again=input("Do you want to continue? (y/n): ".lower())
-        if again not in ["y" and "yes"]:
+        again=input("Do you want to continue? (y/n): ").lower().strip()
+        if again not in ["y","yes"]:
             print("Thank you for your time!")
             break
 
