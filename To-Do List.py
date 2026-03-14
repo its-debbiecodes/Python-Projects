973517328d78a1d6c84e2c7c118c1b80947206ab
its-debbiecodes <debbieabegunde23@gmail.com>
2026-03-14 12:38:13 +0000

    git commit -m "Added all features for TO DO LIST V3.0"

--- To-Do List.py
+++ To-Do List.py
@@ -1,8 +1,27 @@
 
 import os
+import json
+
+FILENAME = "To-Do List.json"
 name = input("Please enter your name: ").title()
+
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def save_tasks(tasks):
+    with open(FILENAME, "w") as f:
+        json.dump(tasks, f)
+
+def load_tasks():
+    if not os.path.exists(FILENAME):
+        return []
+    try:
+        with open(FILENAME, "r") as f:
+            tasks=json.load(f)
+            return tasks
+    except FileNotFoundError:
+        return []
+
 
 def show_menu():
     print("-----YOUR TO DO LISTS-----")
@@ -14,7 +33,7 @@ def show_menu():
     print("5. Exit".capitalize())
 
 def main():
-    tasks=[]
+    tasks=load_tasks()
 
     while True:
         clear_screen()
@@ -30,46 +49,47 @@ def main():
             else:
                 for index, item in enumerate(tasks, start=1):
                     print(f"{index}. {item}")
-                    input("Press enter to go back to main menu...")
+            input("Press enter to go back to continue...")
 
         elif choice == "2":
             new_task = input("Please enter a new task: ")
             tasks.append(new_task)
-
+            save_tasks(tasks)
             print(f"{new_task} added to your tasks list")
-            for index, item in enumerate(tasks, start=1):
-                print(f"{index}. {item}")
+            input("Press enter to go back to continue...")
 
         elif choice == "3":
             if not tasks:
                 print("Your tasks list is empty!")
+            else:
+                for index, item in enumerate(tasks, start=1):
+                    print(f"{index}. {item}")
+                try:
+                    task_num = int(input("Please enter the number of task you would like to remove: "))
+                    removed_task=tasks.pop(task_num-1)
+                    save_tasks(tasks)
+                    print(f"{removed_task} removed from your tasks list")
 
-            for index, item in enumerate(tasks, start=1):
-                print(f"{index}. {item}")
-
-            try:
-                task_num = int(input("Please enter the number of task you would like to remove: "))
-                removed_task=tasks.pop(task_num-1)
-                print(f"{removed_task} removed from your tasks list")
-
-            except (ValueError, IndexError) as e:
-                print(f"{e}:Please enter a valid number from your tasks list")
+                except (ValueError, IndexError) as e:
+                    print(f"{e}:Please enter a valid number from your tasks list")
+            input("Press enter to go back to continue...")
 
         elif choice == "4":
             if not tasks:
                 print("Your tasks list is empty!")
 
-            try:
-                task_num = int(input("Please enter the number of task you would like to mark as done: "))
-                if "✅" not in tasks[task_num-1]:
-                    tasks[task_num - 1]= tasks[task_num-1] + "✅"
-                    print(f"{tasks[task_num-1]} marked as done in your tasks list")
-
-                else:
-                    print(f" {tasks[task_num-1]} Already marked as done in your tasks list")
-
-            except (ValueError, IndexError) as e:
-                print(f"{e}:Please enter a valid number from your tasks list")
+            else:
+                try:
+                    task_num = int(input("Please enter the number of task you would like to mark as done: "))
+                    if "✅" not in tasks[task_num-1]:
+                        tasks[task_num - 1]+= "✅"
+                        save_tasks(tasks)
+                        print(f"{tasks[task_num-1]} marked as done in your tasks list")
+                    else:
+                        print(f" {tasks[task_num-1]} Already marked as done in your tasks list")
+                except (ValueError, IndexError) as e:
+                    print(f"{e}:Please enter a valid number from your tasks list")
+            input("Press enter to go back to continue...")
 
         elif choice == "5":
             print("Goodbye!")
